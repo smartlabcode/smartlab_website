709847686d684a790e356e6c3a51ec3c5171dbd6
Mirza O <oglecevacmirza@gmail.com>
2019-04-23 16:24:33 +0200

    Created BlogTranslation model and set relationships among User Blof and BlogTranslation models

--- app/Blog.php
+++ app/Blog.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function translations() {
+        return $this->hasMany('App\BlogTranslation');
+    }
 }
