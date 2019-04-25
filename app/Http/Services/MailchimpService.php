f4f1adee09700a92d03e3b27054a72b8f7ca5756
Mirza O <oglecevacmirza@gmail.com>
2019-04-25 16:57:48 +0200

    Set mailchimp service class and installed guzzlehttp

---
+++ app/Http/Services/MailchimpService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 25/04/2019
+ * Time: 16:43
+ */
+
+namespace App\Http\Services;
+
+
+class MailchimpService
+{
+
+    public function addSubscriber() {
+
+    }
+}
