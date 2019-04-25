baf685b55bc82808e0696d72ec53bcd60cf27f99
Mirza O <oglecevacmirza@gmail.com>
2019-04-25 13:09:39 +0200

    Created subscriber model and migration

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class SubscribeController extends Controller
 {
     public function saveSubscriber(Request $request) {
-        
+
+        $request->validate([
+            'email' => 'required|max:45'
+        ]);
+
+        return back()->with(['message' => 'You successfully subscribed to our newsletter.']);
     }
 }
