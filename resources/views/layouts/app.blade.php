20d6fd3dcc4e4817175a6aac5a9f93f523176048
thewhitewolf2411 <39027423+thewhitewolf2411@users.noreply.github.com>
2019-05-14 16:54:53 +0200

    Chat početak

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,5 +72,30 @@
         </main>
     </div>
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root" page_id="723581587841509"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v3.3'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+page_id="723581587841509"
+  attribution=setup_tool
+  page_id="723581587841509">
+</div>
+
 </body>
 </html>
