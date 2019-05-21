70ead13389cf38d189a26a8d2a3598a530b46a78
Emir Mekić <emir.mekic@live.com>
2019-05-21 14:56:05 +0200

    some routes added

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -64,17 +64,17 @@
 <li class="nav-item  dropdown">
     <a class="dropdown-toggle nav-link" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">@lang('menu.first_item')</a>
     <div class="dropdown-menu square" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item py-3" href="#">@lang('menu.online_courses')</a>
-        <a class="dropdown-item py-3" href="#">@lang('menu.educational_video')</a>
-        <a class="dropdown-item py-3" href="#">@lang('menu.programming')</a>
-        <a class="dropdown-item py-3" href="#">@lang('menu.moodle')</a>
+        <a class="dropdown-item py-3" href="/pages/courses">@lang('menu.online_courses')</a>
+        <a class="dropdown-item py-3" href="/pages/animations">@lang('menu.educational_video')</a>
+        <a class="dropdown-item py-3" href="/pages/programming">@lang('menu.programming')</a>
+        <a class="dropdown-item py-3" href="/pages/moodle">@lang('menu.moodle')</a>
     </div>
 </li>
 <li class="nav-item dropdown">
     <a class="dropdown-toggle nav-link" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">@lang('menu.second_item')</a>
     <div class="dropdown-menu square">
-        <a class="dropdown-item py-3" href="#">@lang('menu.about_us')</a>
-        <a class="dropdown-item py-3" href="#">@lang('menu.our_team')</a>
+        <a class="dropdown-item py-3" href="#about">@lang('menu.about_us')</a>
+        <a class="dropdown-item py-3" href="#team">@lang('menu.our_team')</a>
     </div>
 </li>
 <li class="nav-item dropdown join ">
@@ -93,7 +93,7 @@
     <!-- Open link in new tab and set its language depending on the current language in main website-->
     <a class="nav-link" href="{{ env("BLOG_DOMAIN")  }}/@if(App::getlocale()){{App::getlocale()}}@else en @endif" target="_blank">@lang('menu.fourth_item')</a>
 </li>
-<li class="nav-item"><a class="nav-link" href="#">@lang('menu.fifth_item')</a></li>
+<li class="nav-item"><a class="nav-link" href="#contact">@lang('menu.fifth_item')</a></li>
 
 <!-- This menu items are available only to logged in users -->
 @auth
