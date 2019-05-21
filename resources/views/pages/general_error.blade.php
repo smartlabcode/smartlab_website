559118eff7dbccfda306d509a9bdd1a5431343de
AjraH <49635717+AjraH@users.noreply.github.com>
2019-05-21 17:02:05 +0200

    finished errors

--- resources/views/pages/general_error.blade.php
+++ resources/views/pages/general_error.blade.php
@@ -12,15 +12,13 @@
             --h2-color: #0f75bc;
             --h1-color: #14213d;
             --p-color: #c9d6e2;
+            --waves:#72c7fa;
+            --sky:#4885fa;
             scroll-behavior: smooth;
         }
-        html {
+        /*html {
             background: url(images/404-illustration.svg) no-repeat center center fixed;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        }
+        }*/
         .h1-font {
             font-family: "Montserrat", sans-serif;
             font-size: 80px;
@@ -36,6 +34,7 @@
         }
 
         .button{
+            font-weight: bold;
             z-index: 5;
             cursor: pointer;
             border-radius: 35px;
@@ -54,25 +53,47 @@
             align-items: center;
             margin-top: 13vh;
         }
+        #image{
+            background: url("images/404-illustration.svg") no-repeat center center;
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+            background-size: 100%;
+        }
+
+        body {
+            margin: 0;
+        }
 
         @media screen and (min-width: 2000px){
             .h1-font {
-                font-size: 160px;
+                font-size: 177px;
+            }
+
+            .sky {
+                height: 0 !important;
             }
 
             .button{
-                width: 300px;
-                height: 90px;
-                font-size: 30px;
+                width: 320px;
+                height: 100px;
+                font-size: 35px;
             }
 
             .h2-font{
-                font-size: 40px;
+                font-size: 55px;
                 margin-top: -8%;
             }
 
             .container {
-                margin-top: 15vh;
+                margin-top: -55%;
+            }
+            #image{
+                margin-top: -1vh;
+            }
+
+            .waves{
+                height: 0 !important;
             }
         }
         @media screen and (max-width: 2000px){
@@ -92,96 +113,189 @@
             }
 
             .container {
-                margin-top: 5vh;
+                margin-top: -58%;
+            }
+
+            .sky {
+                height: 0 !important;
+            }
+
+            .waves {
+                height: 55vh !important;
+                margin-top: 20%;
             }
         }
         @media screen and (max-width: 1500px){
             .h1-font {
-                font-size: 140px;
+                font-size: 90px;
             }
 
             .button{
-                width: 255px;
-                height: 70px;
-                font-size: 28px;
+                width: 175px;
+                height: 55px;
+                font-size: 20px;
             }
 
             .h2-font{
-                font-size: 35px;
-                margin-top: -13%;
+                font-size: 27px;
+                margin-top: -8%;
             }
 
             .container {
-                margin-top: 2vh;
+                margin-top: -55%;
             }
+            .sky {
+                height: 0 !important;
+            }
+
+            .waves {
+                height: 55vh !important;
+            }
+
 
         }
         @media screen and (max-width: 1100px){
             .h1-font {
-                font-size: 100px;
+                font-size: 80px;
             }
 
             .button{
-                width: 246px;
-                height: 70px;
-                font-size: 24px;
+                width: 157px;
+                height: 50px;
+                font-size: 18px;
             }
 
             .h2-font{
-                font-size: 30px;
-                margin-top: -12%;
+                font-size: 24px;
+                margin-top: -10%;
             }
 
+            .container {
+                margin-top: -55%;
+            }
+            .sky {
+                height: 25vh !important ;
+            }
+
+            .waves {
+                height: 55vh !important;
+                margin-top: 23%;
+            }
+            #image {
+                margin-top: -4%;
+            }
+        }
+        @media screen and (max-width: 900px){
+            .sky {
+                height: 25vh !important ;
+            }
+
+            .waves {
+                height: 55vh !important;
+                margin-top: 3%;
+            }
         }
         @media screen and (max-width: 800px) {
             .h1-font {
-                font-size: 77px;
+                font-size: 65px;
+            }
+
+            #image{
+                margin-top: -1%;
             }
 
             .button{
-                width: 220px;
-                height: 65px;
-                font-size: 22px;
+                width: 145px;
+                height: 45px;
+                font-size: 17px;
             }
 
             .h2-font{
-                font-size: 25px;
-                margin-top: -13%;
+                font-size: 20px;
+                margin-top: -10%;
+            }
+
+            .container {
+                margin-top: -60%;
+            }
+            .sky {
+                height: 25vh !important ;
+            }
+
+            .waves {
+                height: 55vh !important;
+                margin-top: 23%;
+            }
+        }
+        @media screen and (max-width: 620px) {
+            .waves{
+                margin-top: 22%;
+                height: 50vh !important;
+            }
+
+            .container {
+                margin-top: -73%;
             }
 
         }
         @media screen and (max-width: 450px) {
             .h1-font {
-                font-size: 77px;
+                font-size: 48px;
             }
 
             .button{
-                width: 170px;
-                height: 60px;
-                font-size: 18px;
+                width: 125px;
+                height: 33px;
+                font-size: 13px;
             }
 
             .h2-font{
-                font-size: 18px;
-                margin-top: -23%;
+                font-size: 16px;
+                margin-top: -15%;
+            }
+
+            .sky {
+                height: 31vh !important ;
+            }
+
+            .waves {
+                height: 65vh !important;
+                margin-top: -5%;
+            }
+
+            .container {
+                margin-top: -90%;
             }
 
         }
         @media screen and (max-width: 376px) {
 
             .h1-font {
-                font-size: 70px;
+                font-size: 45px;
             }
 
             .button{
-                width: 150px;
-                height: 55px;
-                font-size: 18px;
+                width: 105px;
+                height: 30px;
+                font-size: 13px;
             }
 
             .h2-font{
-                font-size: 16px;
-                margin-top: -25%;
+                font-size: 14px;
+                margin-top: -15%;
+            }
+
+            .sky {
+                height: 31vh !important ;
+            }
+
+            .waves {
+                height: 65vh !important;
+                margin-top: -5%;
+            }
+
+            .container {
+                margin-top: -90%;
             }
         }
         @media screen and (max-width: 330px) {
@@ -189,6 +303,10 @@
                 font-size: 70px;
             }
 
+            .container{
+                margin-top: -8vh;
+            }
+
             .button{
                 width: 150px;
                 height: 55px;
@@ -197,27 +315,58 @@
 
             .h2-font{
                 font-size: 16px;
-                margin-top: -30%;
+                margin-top: -16%;
             }
 
+            #image {
+                background-size: contain;
+            }
+            .sky {
+                height: 31vh !important ;
+            }
+
+            .waves {
+                height: 45vh !important;
+                margin-top: 15%;
+            }
+
+        }
+
+        .sky {
+            z-index: -10;
+            background-color:var(--sky);
+            width: 100%;
+            height: 13vh;
+        }
+        .waves {
+            z-index: -10;
+            background-color:var(--waves);
+            width: 100%;
+            height: 13vh;
         }
     </style>
 </head>
 <body>
-<div class="container">
-    <div class="h1-font">
-        <p>OOPS</p>
-    </div>
-
-    <div class="h2-font">
-        <p>There is only a 404 error<br> message ahead!</p>
-    </div>
-
-    <button id="button" class="button" onclick="goHome()">
-        Back to home
-    </button>
+<div class="sky">
 </div>
+<div>
+    <img class="image" id="image" src="{{'/images/404-illustration.svg'}}">
+    <div class="container">
+        <div class="h1-font">
+            <p>OOPS</p>
+        </div>
 
+        <div class="h2-font">
+            <p>There is only a 404 error<br> message ahead!</p>
+        </div>
+
+        <button id="button" class="button" onclick="goHome()">
+            Back to home
+        </button>
+    </div>
+</div>
+<div class="waves">
+</div>
 <script>
     function goHome() {
         document.querySelector(location.href = "/index.php")
