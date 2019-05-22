a99d93f49e0cc9025f3a59a899fb9a8d67a4083e
Emir Mekić <emir.mekic@live.com>
2019-05-22 16:54:56 +0200

    moved navigation to menu.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,85 +41,28 @@
             width: 90%;
             margin: 0 auto;
         }
-
-        /*.navbar {
-            position: fixed;
-            top: 0;
-            width: 100%;
-            z-index: 100;
-        }*/
-
-        .contain {
-            max-width: 1600px;
-            width: 90%;
-            margin: 0 auto;
-        }
-
-        nav .container {
-            padding-right: 0 !important;
-            padding-left: 0 !important;
-            max-width: 1600px;
-            width: 90%;
-            margin: 0 auto;
-        }
-
-
-
-
-        @media screen and (max-width: 990px) {
-            .navbar-brand img {
-                height: 20px;
-            }
-        }
-
-        @media screen and (max-width: 768px) {
-            .navbar-brand img {
-                height: 30px;
-            }
-        }
     </style>
 
 </head>
 
 <body>
-
     <!-- Import menu for including it later -->
     @include('layouts.menu')
     @include('layouts.footer')
     <!-- Main application layout -->
     <div id="app">
-
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel navbar-fixed-top ">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src={{"/images/smartlab-logo.svg"}} height="30px">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav ml-auto">
-
-                        <!-- Include menu-->
-                        @yield('menu')
-
-                    </ul>
-                </div>
-            </div>
+        <nav class="contain">
+            <!-- Include menu-->
+            @yield('menu')
         </nav>
-
         <!-- Content container -->
         <main>
-
             <!-- Include content -->
             @yield('content')
         </main>
         @yield('footer')
     </div>
-
 </body>
 
 </html>
