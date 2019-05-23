d9fcbcac5878c422b42c64103cfc9bb131bda53d
Emir Mekić <emir.mekic@live.com>
2019-05-23 15:36:32 +0200

    navigation on scroll changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -20,9 +20,9 @@
         justify-content: flex-end;
         position: fixed;
         top: 0;
+        width: 100%;
         /* left: 0; */
-        width: 90%;
-        max-width: 1440px;
+        right: 0;
         z-index: 1000;
     }
 
@@ -35,12 +35,24 @@
         width: 400px;
         background-color: white;
         border-radius: 5px;
+        transition: height 0.2s ease-in-out;
     }
+
+    .expandable:hover {
+        height: auto;
+    }
+
+    .nav-li:hover .expandable {
+        height: auto;
+
+    }
+
 
     .expandable a {
         padding: 20px !important;
         transition: background-color 0.2s ease-in-out;
         color: var(--h1-color) !important;
+
     }
 
     .expandable a:hover {
@@ -64,12 +76,24 @@
         font-family: "Montserrat", sans-serif;
     }
 
-    .nav-li a::before {
+    .nav-li-js>a::before {
         content: "";
         display: inline-block;
         position: absolute;
         right: 0;
-        top: 0;
+        top: 5px;
+        height: 1px;
+        width: 0px;
+        background-color: var(--h1-color) !important;
+        transition: width 0.2s ease-in-out;
+    }
+
+    .nav-li>a::before {
+        content: "";
+        display: inline-block;
+        position: absolute;
+        right: 0;
+        top: 5px;
         height: 1px;
         width: 0px;
         background-color: white;
@@ -78,19 +102,35 @@
 
     .nav-li:hover a::before {
         width: 15px;
-
     }
 
     .nav-li a:hover {
         text-decoration: none;
     }
 
-    .nav-li:hover .expandable {
-        height: auto;
-    }
+
 
     .arrow:hover:before {
-        transform: rotate(-180deg) translate(80%, -90%);
+        transform: rotateX(-180deg) translate(-100%, -95%);
+    }
+
+    .arrow-js:hover:before {
+        transform: rotateX(-180deg) translate(-100%, -170%);
+        animation: arrow 0.2s ease-in-out forwards;
+    }
+
+    @keyframes arrow {
+        0% {
+            background-color: var(--h1-color);
+        }
+
+        50% {
+            background-color: var(--h1-color);
+        }
+
+        100% {
+            background-color: white;
+        }
     }
 
     .nav-top {
@@ -111,6 +151,32 @@
         position: relative;
     }
 
+    .filler {
+        position: absolute;
+        width: 100%;
+        height: 15px;
+    }
+
+    .arrow-js {
+        position: relative;
+    }
+
+    .arrow-js::before {
+        display: inline-block;
+        position: absolute;
+        content: "";
+        background-color: #14213d;
+        width: 15px;
+        height: 15px;
+        left: 100%;
+        top: 50%;
+        transform: translate(-100%, -75%);
+        -webkit-clip-path: polygon(0 50%, 50% 100%, 100% 50%);
+        clip-path: polygon(0 50%, 50% 100%, 100% 50%);
+        transition: 0.2s transform ease-in-out;
+
+    }
+
     .arrow::before {
         display: inline-block;
         position: absolute;
@@ -124,6 +190,7 @@
         -webkit-clip-path: polygon(0 50%, 50% 100%, 100% 50%);
         clip-path: polygon(0 50%, 50% 100%, 100% 50%);
         transition: 0.2s transform ease-in-out;
+
     }
 
     .join {
@@ -155,8 +222,8 @@
 @section('menu')
 
 <!-- Available to all users -->
-<ul class="nav-list">
-    <div class="nav-top">
+<ul class="nav-list " id="nav-list">
+    <div class="nav-top contain" id="nav-top">
 
         <p>Call: <span>+387 61 811 394</span> <span>+387 33 956 222</span></p>
         <form id="languageForm" action="/language" method="POST">
@@ -172,11 +239,12 @@
 
     </div>
 
-    <div class="nav-bot">
+    <div class="nav-bot contain">
         <a href="/"><img class="nav-logo" src={{"/images/smartlab-logo.svg"}}></a>
 
-        <li class="nav-li arrow">
-            <a href="#">@lang('menu.first_item')</a>
+        <li class="nav-li nav-li-js arrow">
+            <a href="#" class="grey">@lang('menu.first_item')</a>
+            <div class="filler"></div>
             <div class="expandable">
                 <a href="/pages/courses">@lang('menu.online_courses')</a>
                 <a href="/pages/animations">@lang('menu.educational_video')</a>
@@ -184,15 +252,17 @@
                 <a href="/pages/moodle">@lang('menu.moodle')</a>
             </div>
         </li>
-        <li class="nav-li arrow">
-            <a href="#">@lang('menu.second_item')</a>
+        <li class="nav-li nav-li-js arrow">
+            <a href="#" class="grey">@lang('menu.second_item')</a>
+            <div class="filler"></div>
             <div class="expandable">
                 <a href="/#about">@lang('menu.about_us')</a>
                 <a href="/#team">@lang('menu.our_team')</a>
             </div>
         </li>
-        <li class="nav-li arrow">
-            <a href="#">@lang('menu.third_item')</a>
+        <li class="nav-li nav-li-js arrow">
+            <a href="#" class="grey">@lang('menu.third_item')</a>
+            <div class="filler"></div>
             <div class="expandable join">
                 <div class="join-left">
                     <a href="#">@lang('menu.outsourcing')</a>
@@ -203,38 +273,38 @@
                 </div>
             </div>
         </li>
-        <li class="nav-li">
+        <li class="nav-li nav-li-js">
             <!-- Open link in new tab and set its language depending on the current language in main website-->
-            <a href="{{ env("BLOG_DOMAIN")  }}/@if(App::getlocale()){{App::getlocale()}}@else en @endif" target="_blank">@lang('menu.fourth_item')</a>
+            <a class="padding-right-0 grey" href="{{ env("BLOG_DOMAIN")  }}/@if(App::getlocale()){{App::getlocale()}}@else en @endif" target="_blank">@lang('menu.fourth_item')</a>
         </li>
-        <li class="nav-li"><a class="padding-right-0" href="#">@lang('menu.fifth_item')</a></li>
+        <li class="nav-li nav-li-js"><a class="padding-right-0 grey" href="#">@lang('menu.fifth_item')</a></li>
 
         <!-- This menu items are available only to logged in users -->
         @auth
 
         <!-- This menu item is available to super admins only (list of all admins) -->
         {{--@if(\Illuminate\Support\Facades\Auth::user()->roles_id == 1)--}}
-        <li class="nav-li">
-            <a href="/dashboard">@lang('menu.sixth_item')</a>
+        <li class="nav-li nav-li-js">
+            <a class="grey padding-right-0" href="/dashboard">@lang('menu.sixth_item')</a>
         </li>
         {{--@endif--}}
 
-        {{--<li class="nav-li">--}}
-        {{--<a href="{{route('blogs.index')}}">@lang('menu.seventh_item')</a>--}}
+        {{--<li class="nav-li nav-li-js">--}}
+        {{--<a class="grey padding-right-0" href="{{route('blogs.index')}}">@lang('menu.seventh_item')</a>--}}
         {{--</li>--}}
 
         <!-- This menu item is available to super admins only (list of all subscribers) -->
         {{--@if(\Illuminate\Support\Facades\Auth::user()->roles_id == 1)--}}
-        {{--<li class="nav-li">--}}
-        {{--<a href="/subscribers">@lang('menu.eight_item')</a>--}}
+        {{--<li class="nav-li nav-li-js">--}}
+        {{--<a class="grey padding-right-0" href="/subscribers">@lang('menu.eight_item')</a>--}}
         {{--</li>--}}
         {{--@endif--}}
 
         <!-- List of all logs in the system -->
-        {{--<li class="nav-li"><a href="/logs">@lang('menu.ninth_item')</a></li>--}}
+        {{--<li class="nav-li nav-li-js"><a class="grey padding-right-0" href="/logs">@lang('menu.ninth_item')</a></li>--}}
 
         <!-- Link for logging out -->
-        <li class="nav-li"><a href="{{route('logout')}}">@lang('menu.tenth_item')</a></li>
+        <li class="nav-li nav-li-js"><a class="grey padding-right-0" href="{{route('logout')}}">@lang('menu.tenth_item')</a></li>
 
         @endauth
     </div>
@@ -244,5 +314,53 @@
 
 
 </ul>
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        let nav = document.querySelector("#nav-list");
+        let navTop = document.querySelector("#nav-top");
+        let grey = document.querySelectorAll(".grey");
+        let arrow = document.querySelectorAll(".arrow");
+        let expandable = document.querySelectorAll(".expandable");
+        let navLi = document.querySelectorAll(".nav-li");
+        console.log(navLi);
+        window.addEventListener("scroll", function(event) {
+            console.log(window.pageYOffset);
+            if (window.pageYOffset > 300) {
+                nav.style.backgroundColor = "white";
+                navTop.style.display = "none";
+                for (let i = 0; i < arrow.length; i++) {
+                    arrow[i].classList.remove("arrow");
+                    arrow[i].classList.add("arrow-js");
+                }
+                for (let i = 0; i < grey.length; i++) {
+                    grey[i].style.color = "#14213d";
+                }
+                for (let i = 0; i < expandable.length; i++) {
+                    expandable[i].style.top = "54px";
+                }
+                for (let i = 0; i < navLi.length; i++) {
 
+                    navLi[i].classList.add("nav-li-js");
+                }
+            } else {
+                nav.style.backgroundColor = "transparent";
+                navTop.style.display = "flex";
+                for (let i = 0; i < arrow.length; i++) {
+                    arrow[i].classList.add("arrow");
+                    arrow[i].classList.remove("arrow-js");
+                }
+                for (let i = 0; i < grey.length; i++) {
+                    grey[i].style.color = "white";
+                }
+                for (let i = 0; i < expandable.length; i++) {
+                    expandable[i].style.top = "auto";
+                }
+                for (let i = 0; i < navLi.length; i++) {
+                    navLi[i].classList.remove("nav-li-js");
+
+                }
+            }
+        })
+    });
+</script>
 @endsection
