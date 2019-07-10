220066b6b6a06711dab0f20d2777464e545b8e0a
Emir Mekić <emir.mekic@live.com>
2019-07-10 15:19:32 +0200

    Update test.js

--- public/js/test.js
+++ public/js/test.js
@@ -4,20 +4,29 @@
 let button = document.querySelectorAll(".button");
 let profileImg = document.querySelectorAll(".profile-image");
 let description = document.querySelectorAll(".team-description");
+let descriptionLong = document.querySelectorAll(".team-description-long");
+let skills = document.querySelectorAll(".team-skills");
 let team = [];
 let sessionImg = [];
 let sessionDesc = [];
+let sessionDescLong = [];
+let sessionSkills = [];
+
 class Team {
-    constructor(image, description) {
+    constructor(image, description, descriptionLong, skills) {
         this.image = image;
         this.description = description;
+        this.descriptionLong = descriptionLong;
+        this.skills = skills;
     }
 }
 
 
 for (let i = 0; i < button.length - 1; i++) {
-    team[i] = new Team(profileImg[i].src, description[i].innerHTML);
+    team[i] = new Team(profileImg[i].src, description[i].innerHTML, descriptionLong[i].innerHTML, skills[i]);
     sessionImg[i] = sessionStorage.setItem("teamImg" + i, team[i].image);
     sessionDesc[i] = sessionStorage.setItem("teamDesc" + i, team[i].description);
+    sessionDescLong[i] = sessionStorage.setItem("teamDescLong" + i, team[i].descriptionLong);
+    sessionSkills[i] = sessionStorage.setItem("teamSkills" + i, team[i].skills.innerHTML);
+    console.log(skills[i].innerHTML)
 }
-//export default team;
